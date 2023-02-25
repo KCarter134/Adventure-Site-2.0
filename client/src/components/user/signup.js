9733a22f541689c483999ba0676ab25ee6845ca8
Kyle Carter <kcarter134@yahoo.com>
2023-02-25 11:22:40 -0500

    uncontrolled form handling

--- client/src/components/user/signup.js
+++ client/src/components/user/signup.js
@@ -1,70 +1,57 @@
 import mountainPic from '../../assets/pictures/mountain.jpg'
 import { Link } from 'react-router-dom'
-import React, { useState, useRef, Component } from 'react'; 
+import React, { useState, useRef } from 'react'; 
 
 
-class Signup extends Component {
-    state = {
-        tagged: false,
-        message: '',
-        name: ''
-    }
-
-    handleClick(e) {
-        // access input values in the state
-        console.log(this.state) // {tagged: true, input1: 'text', input2: 'text2'}
-        this.setState({tagged: true});
-        e.preventDefault();
-        console.log('The link was clicked.');
-    }
-
-    handleInputChange = (e, input) => {
-        this.setState({
-         [input]: e.target.value
-       })
-      }
-
+export default function Signup() {
     // const [updated, setUpdated] = useState('');
     // const inputRef = useRef(null);
 
+  const fNameRef = useRef();
+  const lNameRef = useRef();
+  const uNameRef = useRef();
+  const emailRef = useRef();
+  const phoneRef = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(fNameRef.current.value);
+    console.log(lNameRef.current.value);
+    console.log(uNameRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(phoneRef.current.value);
+  }
     // const handleSubmit = () => {
-    //     this.preventDefault();
-    //     setUpdated(inputRef.current.value).innerHTML;
+    //     setUpdated(inputRef.current.value);
     // }
 
 
-    render() {
-    return (
-            <section className="login-signup-main">
-                <img src={mountainPic} alt="" className='background-pic' />
-                <div className='login-back-plate'></div>
-                <form className='login-info-plate'>
-                    <Link to="/login"
-                        id='to-signin'
-                        className='login-f-btns'> ← go to login
-                        </Link>
-                    <div className='login-title'>Signup</div>
-                    <div className='login-info-flex'>
-                        <input type="text" onChange={(e) => this.handleInputChange(e, 'input1')} className='login-info' name='fname' placeholder='First Name' />
-                        <input type="text" className='login-info' name='lname' placeholder='Last Name' />
-                        <input type="text" className='login-info' name='uname' placeholder='Username' />
-                        <input type="text" className='login-info' name='email' placeholder='Email' />
-                        <input type="text" className='login-info' name='phone' placeholder='Phone Number' />
-                    </div>
-                    <div className='submit-button-flex'>
-                    <button
-                    onClick={(e) => this.handleClick(e)}
-                    >
-                    {this.state.tagged ? 'Tagged' : 'Tag ' } 
-                </button>
-                    </div> 
-                    <p>
-                    {this.state.tagged ? 'Clicked' : 'Still' }
-                </p>
-                </form>
-            </section>
-        )
-    }
-}
 
-export default Signup;
+    return (
+        <section className="login-signup-main">
+            <img src={mountainPic} alt="" className='background-pic' />
+            <div className='login-back-plate'></div>
+            <form className='login-info-plate' onSubmit={onSubmit}>
+                <Link to="/login"
+                    id='to-signin'
+                    className='login-f-btns'> ← go to login
+                    </Link>
+                <div className='login-title'>Signup</div>
+                <div className='login-info-flex'>
+                    <input type="text" ref={fNameRef} className='login-info' name='fname' placeholder='First Name' />
+                    <input type="text" ref={lNameRef} className='login-info' name='lname' placeholder='Last Name' />
+                    <input type="text" ref={uNameRef} className='login-info' name='uname' placeholder='Username' />
+                    <input type="text" ref={emailRef} className='login-info' name='email' placeholder='Email' />
+                    <input type="text" ref={phoneRef} className='login-info' name='phone' placeholder='Phone Number' />
+                </div>
+                <div className='submit-button-flex'>
+                    <input 
+                    id='signup-submit' 
+                    type='submit'
+                    className='login-f-btns'
+                    value="submit" />
+                </div> 
+            </form>
+        </section>
+    )
+}
